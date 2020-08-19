427eb1468adccb421db9f8cb7f6f0697d544115d
zhal73 <domenico.gagliano1973@gmail.com>
2020-08-19 12:04:36 +0000

    update requirement

---
+++ service1/tests/test_service1.py
@@ -0,0 +1,19 @@
+from flask import url_for
+from flask_testing import TestCase
+
+from application import app
+
+class TestBase(TestCase):
+
+    def create_app(self):
+        config_name = 'testing'
+        return app
+
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+##### TESTS IMPLEMENTATION  ########
+
+
